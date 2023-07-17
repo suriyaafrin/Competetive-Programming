edaaeb3eb098eea0b2facf78b026a4a87bd0dd33
suriyaafrin <108604780+suriyaafrin@users.noreply.github.com>
2023-07-17 21:09:12 +0600

    afrin

---
+++ codechef-solution/Age Limit.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int x,y,a;
+    cin>>x>>y>>a;
+    if(a>=x && a<y)
+    {
+       cout<<"YES\n";
+
+    }
+    else{
+        cout<<"NO\n";
+    }
+    }
+    return 0;
+}
