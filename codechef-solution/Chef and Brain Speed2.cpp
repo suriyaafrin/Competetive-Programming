36c5c2b3da60898c9df1ac0ad766498041781d24
suriyaafrin <108604780+suriyaafrin@users.noreply.github.com>
2023-07-19 21:33:53 +0600

    afrin

---
+++ codechef-solution/Chef and Brain Speed2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(x<y)
+    {
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+
+
+
+    return 0;
+}
