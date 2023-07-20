52b1c5cbf0357a5b969f4b7bc98a9c467e18bf15
suriyaafrin <108604780+suriyaafrin@users.noreply.github.com>
2023-07-20 19:14:58 +0600

    afrin

---
+++ codechef-solution/Puzzle Hunt.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>=6 && x<=8)
+    {
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+
+
+    return 0;
+}
