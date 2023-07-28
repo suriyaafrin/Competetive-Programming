afd4af70e3773c30a67ffe1146a43648a109c0f0
suriyaafrin <108604780+suriyaafrin@users.noreply.github.com>
2023-07-28 22:32:42 +0600

    afrin

---
+++ codechef-solution/Passes for Fair.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        if(k>n) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
