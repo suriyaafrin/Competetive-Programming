ca0de266e782f1dc6926603919f26fcb96a15835
suriyaafrin <108604780+suriyaafrin@users.noreply.github.com>
2023-08-01 21:09:11 +0600

    Create BinarySearch.cpp
    afrin

---
+++ important code/BinarySearch.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+     cin>>arr[i];
+    }
+    int ele;
+    cin>>ele;
+
+    int left = 0;
+    int right =n-1;
+    int index = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == ele) {
+            index = mid;
+            break;
+        } else if (arr[mid] < ele) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
